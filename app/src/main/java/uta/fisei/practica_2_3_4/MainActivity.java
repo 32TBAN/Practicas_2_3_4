557661bc6835814a3f31ad9e761be9f1ade8a51f
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-10-16 11:37:00 -0500

    commit 0

---
+++ app/src/main/java/uta/fisei/practica_2_3_4/MainActivity.java
@@ -0,0 +1,21 @@
+package uta.fisei.practica_2_3_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclickPractica2(View view){
+        Intent intent = new Intent(this,Practica_2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/uta/fisei/practica_2_3_4/Practica_2.java
@@ -0,0 +1,14 @@
+package uta.fisei.practica_2_3_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Practica_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practica2);
+    }
+}
